dc00ec29d8e86fc5eae29fdba50f9ffe39fe7e92
mitnick78 <mchristophe7893@gmail.com>
2026-06-22 16:08:13 +0200

    fix: update sitemap URLs to correct project domain

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,19 +3,19 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://wise-tripfr.vercel.app/",
+      url: "https://wise-tripfr-3jemjtr7d-mitnick78s-projects.vercel.app/",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
     },
     {
-      url: "https://wise-tripfr.vercel.app/ogin",
+      url: "https://wise-tripfr-3jemjtr7d-mitnick78s-projects.vercel.app/login",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.8,
     },
     {
-      url: "https://wise-tripfr.vercel.app/",
+      url: "https://wise-tripfr-3jemjtr7d-mitnick78s-projects.vercel.app/",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.8,
